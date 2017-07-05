2a58e9ee68be57a68f97ada714b375a7b19edc09
Florian Demmer <fdemmer@gmail.com>
2017-07-05 15:35:16 +0200

    Set version 1.0.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='airavata',
-    version='0.9.5',
+    version='1.0.0',
     packages=['airavata', 'airavata.management.commands', 'airavata.management',
               'airavata.templatetags', 'airavata.migrations'],
     include_package_data=True,
